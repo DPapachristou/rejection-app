88e8ea2c84507172f38bdef3271067cba7cfad51
DPapachristou <d.k.papachristou@gmail.com>
2026-03-15 16:10:20 +0200

    small UI fixes

--- frontend/app/components/organisms/Header.vue
+++ frontend/app/components/organisms/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-brand-gray text-white py-5 px-5 text-center gap-10 flex flex-col items-center">
-    <h3 class="text-4xl font-bold">Why Was I Rejected?</h3>
     <span class="text-2xl text-white/40">Turn interview silence into clear,<br/> actionable answers.</span>
+    <img src="/icons/rejectiongeniecopy.svg" alt="Genie Logo" class="w-100 h-20" />
   </div>
 </template>
 
--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="bg-brand-gray flex flex-col items-center min-h-dvh">
-    <span class="py-4 text-white">WhyRejected Logo</span>
+    <img src="/icons/genielogo.svg" alt="Genie Logo" class="w-25 h-25" />
     <div class="w-3/4 h-px bg-gray-500/40"></div>
     <div class="flex-1 w-full flex flex-col items-center justify-center">
       <slot />
     </div>
     <div class="w-3/4 h-px bg-gray-500/40"></div>
     <div class="flex flex-row justify-between items-center w-3/4">
-    <span class="py-4 text-white">WhyRejected Logo</span>
+    <img src="/icons/genielogo.svg" alt="Genie Logo" class="w-25 h-25" />
     <div class="flex flex-row gap-2 items-center">
       <img src="/icons/registered-trademark.svg" alt="trademark" class="w-4 h-4" />
-      <span class="py-4 text-white">WhyRejectedApp 2025</span>
+      <span class="py-4 text-white">RejectionGenie 2026</span>
     </div>
     </div>
   </div>
